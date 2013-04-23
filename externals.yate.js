fec49df59e50e6f57d30a5407dbad3295dd548ee
mctep <mctep@yandex-team.ru>
2013-04-23 19:17:30 +0400

    remove underscore deps

--- externals.yate.js
+++ externals.yate.js
@@ -1,5 +1,22 @@
 (function() {
 
+function extend(obj) {
+    var exts = Array.prototype.slice.call(arguments, 1);
+
+    if (exts.length) {
+        for (var i = 0, c = exts.length; i < c; i++) {
+            var ext = exts[i];
+
+            for (var key in ext) {
+                var prop = ext[key];
+                obj[key] = prop;
+            }
+        }
+    }
+
+    return obj;
+}
+
 yr.externals['nb-extend'] = function(parent, node) {
     if (node[0]) {
         if (typeof node == 'string') {
@@ -7,7 +24,7 @@ yr.externals['nb-extend'] = function(parent, node) {
         } else {
             var dataParent = parent[0].data;
             var dataNode = node[0].data;
-            var dataNew = _.extend(dataParent, dataNode);
+            var dataNew = extend(dataParent, dataNode);
             parent[0].data = dataNew;
         }
     }
