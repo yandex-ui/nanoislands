97284c8837af625f9cd0a31ca8cab3fccf06d74c
vadim <basvasilich@yandex-team.ru>
2013-04-26 22:13:03 +0400

    CHEMODAN-10128 - Дропдаун в nanoislands -> popup

--- nanoislands.js
+++ nanoislands.js
@@ -1,5 +1,7 @@
 include('nanoblocks/nanoblocks.js')
 
+
 include('blocks/button/button.js')
 include('blocks/select/select.js')
 include('blocks/radio-button/radio-button.js')
+include('blocks/popup/popup.js')
