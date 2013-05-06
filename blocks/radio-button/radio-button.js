6ca1060facfe96b936e25e76c82632331823f418
mctep <mctep@yandex-team.ru>
2013-05-06 15:40:45 +0400

    radio-buttons

--- blocks/radio-button/radio-button.js
+++ blocks/radio-button/radio-button.js
@@ -1,22 +1,22 @@
 nb.define('radio-button', {
-    events: {
-        'init': 'init',
-        'click .nb-button': 'select'
-    },
+    // events: {
+    //     'init': 'init',
+    //     'click .nb-button': 'select'
+    // },
 
-    init: function() {
-        this.$node = $(this.node);
-        this.$buttons = this.$node.children('.nb-button');
-    },
+    // init: function() {
+    //     this.$node = $(this.node);
+    //     this.$buttons = this.$node.children('.nb-button');
+    // },
 
-    select: function(e, button) {
-        this.deselectAll();
-        var $button = $(button);
-        $button
-            .addClass('nb-button_checked')
-    },
+    // select: function(e, button) {
+    //     this.deselectAll();
+    //     var $button = $(button);
+    //     $button
+    //         .addClass('nb-button_checked')
+    // },
 
-    deselectAll: function() {
-        this.$buttons.removeClass('nb-button_checked');
-    }
+    // deselectAll: function() {
+    //     this.$buttons.removeClass('nb-button_checked');
+    // }
 });
