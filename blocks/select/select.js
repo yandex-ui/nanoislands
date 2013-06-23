cf9e68b1b79eb7d3dbf4f664b73c6ab494230d40
vadim <basvasilich@yandex-team.ru>
2013-06-23 12:42:36 +0400

    select

--- blocks/select/select.js
+++ blocks/select/select.js
@@ -6,9 +6,9 @@ nb.define('select', {
         'init': function(){
             var that = this
             nb.init(that)
-            var c = that.children()
 
-            console.log(c)
+            // find elements and values
+            var c = that.children()
 
             that.button = c[0]
             that.popup = c[1]
@@ -16,10 +16,10 @@ nb.define('select', {
             that.value = that.$fallback.find('option[selected]').attr('value')
             that.text = that.$fallback.find('option[selected]').html()
 
-            $(that.popup.node).css({
-                'min-width': $(that.button.node).outerWidth() - 2
-            })
+            // preparing control depending on configuration and content
+           this.controlPrepare()
 
+            // subscribe through space to the event from a child popups
             nb.on('select:'+ that.popup.node.getAttribute('id') +':change', function(name, params){
                 that.trigger('changeValue', params)
             })
@@ -27,9 +27,29 @@ nb.define('select', {
         'changeValue': 'onChangeValue'
 
     },
+
+    /**
+     * preparing control depending on configuration and content
+     */
+    controlPrepare: function(){
+        // minimum width of the popup set to the size of the button
+        $(that.popup.node).css({
+            'min-width': $(that.button.node).outerWidth() - 2
+        })
+    },
+
+    /**
+     * Changes a value of control, text on the button and select value it the fallback
+     *
+     * @param name — event id that caused the change
+     * @param params — {
+     *     text: '..'
+     *     value: '..'
+     * }
+     */
     onChangeValue: function(name, params){
-        this.value =  params.value
-        this.text =  params.text
+        this.value = params.value
+        this.text = params.text
         this.button.trigger('textChange', params)
         this.$fallback.find('option[selected]').removeAttr('selected')
         this.$fallback.find('option[value = '+ params.value +']').attr('selected', 'selected')
