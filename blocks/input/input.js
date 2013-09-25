a5edb4eb6fffe30028f5db746a4f0623bdd8d163
vadim <basvasilich@yandex-team.ru>
2013-09-26 01:17:55 +0400

    disable/enable/changeValue on  input

--- blocks/input/input.js
+++ blocks/input/input.js
@@ -1,35 +1,74 @@
 nb.define('input', {
     events: {
-             'init': 'oninit',
-             'click': 'makeFocus',
-             'focusout': 'blur',
-             'focusin': 'makeFocus'
-         },
+        'init': 'oninit',
+        'click': 'makeFocus',
+        'changeValue': 'onChangeValue',
+        'focusout': 'blur',
+        'focusin': 'makeFocus',
+        'disable': 'onDisable',
+        'enable': 'onEnable'
+    },
 
-        oninit: function(){
-            this.$node = $(this.node);
-            this.focused = false;
-            nb.on('input-focusout', function() {
-                 this.trigger('focusout');
+    oninit: function() {
 
-            });
-        },
+        this.$node = $(this.node);
+        this.$nodeInput = this.$node.find('.nb-input__input')
+        this.value = this.$nodeInput.val();
+        this.focused = false;
+        nb.on('input-focusout', function() {
+            this.trigger('focusout');
 
-        makeFocus: function(){
-            if (this.$node.is('.nb-input_disabled')) {
-                return false;
-            }
+        });
+    },
 
-            if (!this.$node.hasClass('nb-input_focus')) {
-                nb.trigger('input-focusout');
-                this.$node.addClass('nb-input_focus');
-                this.focused = true;
-                this.$node.find('input').get(0).focus();
-            }
-        },
-
-        blur: function() {
-            this.$node.removeClass('nb-input_focus');
-            this.focused = false;
+    makeFocus: function() {
+        if (this.$node.is('.nb-input_disabled')) {
+            return false;
         }
+
+        if (!this.$node.hasClass('nb-input_focus')) {
+            nb.trigger('input-focusout');
+            this.$node.addClass('nb-input_focus');
+            this.focused = true;
+            this.$node.find('input').get(0).focus();
+        }
+    },
+
+    blur: function() {
+        this.$node.removeClass('nb-input_focus');
+        this.focused = false;
+    },
+
+    /**
+     * Disables the input
+     */
+    onDisable: function() {
+        this.$node.addClass('nb-input_disabled');
+        this.$nodeInput.attr('disabled', 'disabled');
+        this.trigger('disabled');
+    },
+
+    /**
+     * Enables the input
+     */
+    onEnable: function() {
+        this.$node.removeClass('nb-input_disabled');
+        this.$nodeInput.removeAttr('disabled');
+        this.trigger('enabled');
+    },
+
+    /**
+     * Changes a value of input
+     *
+     * @param name — event id that caused the change
+     * @param params — {
+     *     value: '..'
+     * }
+     */
+    onChangeValue: function(name, params) {
+        this.value = params.value;
+        this.$nodeInput.val(this.value)
+    }
+
+
 });
