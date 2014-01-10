12330c856eacb14eb2798fbad5c4c9df7d714fa3
Vadim Patsev <basvasilich@yandex.ru>
2014-01-10 12:04:11 +0400

    Progress API

--- unittests/spec/progress.js
+++ unittests/spec/progress.js
@@ -1,32 +1,32 @@
 describe('Progress Tests', function() {
     beforeEach(function() {
-        this.progress = nb.find('progress');
+        this.nbProgress = nb.find('progress');
     });
     afterEach(function() {
-        delete this.progress;
+        delete this.nbProgress;
     });
 
 
     describe('#setValue()', function() {
         it('should change the value', function() {
 
-            this.progress.setValue(30);
+            this.nbProgress.setValue(30);
 
-            expect(this.progress.$control.val()).to.equal('30');
+            expect(this.nbProgress.$control.val()).to.equal('30');
         });
 
         it('should change bar width', function() {
-            var control = this.progress.setValue(10);
+            var control = this.nbProgress.setValue(10);
             expect(control.$bar.attr('style').replace(/\s/g, '')).to.equal("width:10%;");
         });
 
         it('should throws nb-progress_value-set event', function() {
             var checked = false;
-            this.progress.on('nb-progress_value-set', function() {
+            this.nbProgress.on('nb-progress_value-set', function() {
                 checked = true;
             });
 
-            this.progress.setValue(48);
+            this.nbProgress.setValue(48);
 
             expect(checked).to.be.ok();
         });
@@ -34,25 +34,25 @@ describe('Progress Tests', function() {
 
     describe('#getValue()', function() {
         it('should return the right value', function() {
-            this.progress.setValue(45);
-            expect(this.progress.getValue()).to.be.equal('45');
+            this.nbProgress.setValue(45);
+            expect(this.nbProgress.getValue()).to.be.equal('45');
         });
     });
 
     describe('#tick()', function() {
         it('should return the right value', function() {
-            this.progress.setValue(45);
-            this.progress.tick();
-            expect(this.progress.getValue()).to.equal('46');
+            this.nbProgress.setValue(45);
+            this.nbProgress.tick();
+            expect(this.nbProgress.getValue()).to.equal('46');
         });
 
         it('should throws nb-progress_value-changed event', function() {
             var checked = false;
-            this.progress.on('nb-progress_value-changed', function() {
+            this.nbProgress.on('nb-progress_value-changed', function() {
                 checked = true;
             });
 
-            this.progress.tick();
+            this.nbProgress.tick();
 
             expect(checked).to.ok();
         });
