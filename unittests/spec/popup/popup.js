e29538d540285e04b7404121778e8a24dfa93dc3
Vadim Patsev <basvasilich@yandex.ru>
2014-01-30 22:06:32 +0400

    Popup refactoring

--- unittests/spec/popup/popup.js
+++ unittests/spec/popup/popup.js
@@ -1,41 +1,132 @@
 describe("Popup Tests", function() {
+
     beforeEach(function() {
         var result = yr.run('main', {popup: true});
         $('.content').html(result);
 
         nb.init();
+
         this.popup = nb.find('popup');
+        this.toggler = nb.find('popup-toggler');
     });
 
     afterEach(function() {
-        delete this.popup;
+        this.popup.destroy();
+        this.toggler.destroy();
     });
 
     describe("Init", function() {
-        it("should be inited", function() {
+        it("Init popup", function() {
             expect(this.popup).to.not.equal(null);
+        });
+        it("Init toggler", function() {
+            expect(this.toggler).to.not.equal(null);
         });
     });
 
-    describe("Events", function() {
-        it("should fire nb-opened event on open", function() {
-            var result = false;
-            this.popup.on('nb-opened', function(){
-                result = true;
-            });
-
-            this.popup.open();
-            expect(result).to.be.ok();
+    describe("#Toggler", function() {
+        it("#Open", function() {
+            this.toggler.open();
+            expect(this.popup.$node.css('display')).to.equal('block');
         });
 
-        it("should fire nb-closed event on close", function() {
-            var result = false;
-            this.popup.on('nb-closed', function(){
-                result = true;
+        it("#Open check event", function() {
+            var flag = false;
+            this.toggler.on('nb-opened', function() {
+                flag = true;
             });
 
-            this.popup.close();
-            expect(result).to.be.ok();
+            this.toggler.open();
+            expect(flag).to.ok();
+        });
+
+        it("#Close event", function() {
+            var flag = false;
+            this.toggler.on('nb-closed', function() {
+                flag = true;
+            });
+
+            this.toggler.close();
+            expect(flag).to.ok();
+        });
+
+        it("#setPopup", function() {
+            this.toggler.setPopup('popup1');
+            this.toggler.open();
+            expect(nb.find('popup1').$node.css('display')).to.equal('block');
+        });
+
+        it("#setPopup check event", function() {
+            var flag = false;
+            this.toggler.on('nb-popup-set', function() {
+                flag = true;
+            });
+
+            this.toggler.setPopup('popup1');
+            expect(flag).to.ok();
+        });
+
+        it("#getPopup", function() {
+            this.toggler.getPopup();
+            expect(this.toggler.getPopup()).to.not.equal(null);
+        });
+
+
+        it("#setOptions", function() {
+            this.toggler.setOptions({where: '.content'});
+            expect(this.toggler.options.where).to.equal('.content');
+        });
+
+        it("#setOptions check event", function() {
+            var flag = false;
+            this.toggler.on('nb-options-set', function() {
+                flag = true;
+            });
+
+            this.toggler.setOptions({where: '.content'});
+            expect(flag).to.ok();
+        });
+
+
+        it("#getOptions", function() {
+            this.toggler.setOptions({where: '.content'});
+            expect(this.toggler.getOptions().where).to.equal('.content');
+        });
+
+        it("should destroy nb.block", function() {
+            this.toggler.destroy();
+            expect(nb.hasBlock($('#popup-toggler')[0])).to.be.equal(false);
+        });
+    });
+
+    describe("#Popup", function() {
+        it("#Open", function() {
+            this.popup.open({where: this.toggler.node, appendTo: '.content'});
+            expect(this.popup.$node.css('display')).to.equal('block');
+        });
+
+        it("#Open check event", function() {
+            var flag = false;
+            this.popup.on('nb-opened', function() {
+                flag = true;
+            });
+
+            this.popup.open({where: this.toggler.node, appendTo: '.content'});
+            expect(flag).to.ok();
+        });
+    });
+
+    describe("#Popup destroy()", function() {
+
+
+        it("should call $.fn.nbContextDialog('destroy')", function() {
+            this.popup.destroy();
+            expect(this.popup.node.widget).to.not.ok();
+        });
+
+        it("should destroy nb.block", function() {
+            this.popup.destroy();
+            expect(nb.hasBlock($('#popup')[0])).to.be.equal(false);
         });
     });
 });
