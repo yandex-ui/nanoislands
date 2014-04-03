d8c5a1ca4345f7d0809a2f04681866e4c333fb7a
Vadim Patsev <basvasilich@yandex.ru>
2014-04-03 18:10:14 +0400

    fix #283

--- build/build-styl.js
+++ build/build-styl.js
@@ -13,12 +13,23 @@ var style = stylus(content)
     .define('url', stylus.resolver());
 
 style.render(function(err, css) {
+    var browsers = [
+        // эти браузеры прибиты по версиям
+        'Explorer >= 9',
+        'Opera 12',
+        'Safari >= 6',
+
+        // эти браузеры обновляются автоматически
+        'last 5 Chrome versions',
+        'last 5 Firefox versions',
+        'last 5 Opera versions'
+    ].join(',')
+    
     if (err) {
         console.error(err);
         process.exit(1);
     }
-    if (!ie) {
-        css = autoprefixer.process(css).css;
-    }
+    
+    css = autoprefixer(browsers).process(css).css;
     process.stdout.write(css);
 });
