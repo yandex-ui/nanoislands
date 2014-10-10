2e040a0c23fb1156e88a91795beaca2ff887ef44
Egor Kopylov <iegit@37.140.174.4-red3.dhcp.yndx.net>
2014-10-10 15:57:13 +0400

    added grunt cli api

--- regression-tests/run.js
+++ regression-tests/run.js
@@ -1,41 +1,48 @@
-var capture = require('./capture');
-var diff = require.bind(require, './diff');
-var exec = require('child_process').exec;
 
+// exporting function or executing it immediately
 if(require.main === module) {
-	var len = process.argv.length;
-	if (len <= 2) return console.log('not enough params');
-
-	run(process.argv[2], process.argv.slice(3));
+    var len = process.argv.length;
+    if (len <= 2) return console.log('not enough params');
+    run(process.argv[2], process.argv.slice(3));
 
 } else {
-	module.exports = run;
+    // changing working directory in order to use reference paths
+    var dir = module.id.replace(/\/.[^\/]+$/, ''); //trimming file name and getting a resolved working directory
+    process.chdir(dir);
+    module.exports = run;
 }
 
-function run (operation, blocks) {
+
+
+// operation: update or diff
+// blocks is the array of blocks names
+function run (operation, blocks, done) {
+    var capture = require('./capture');
+    var diff = require.bind(require, './diff');
+    var exec = require('child_process').exec;
     blocks = blocks || [];
 
     var filenames;
     if (blocks.length === 0) {
-    	filenames = '*';
-    	blocks = require('fs').readdirSync('../blocks');
+        filenames = '*';
+        blocks = require('fs').readdirSync('../blocks');
     }
-	else {
-		filenames = blocks.map(function (b) {
-	    	return b + '\.*.png'
-	    }).join(' ');
-	}
+    else {
+        filenames = blocks.map(function (b) {
+            return b + '\.*.png'
+        }).join(' ');
+    }
 
     switch (operation) {
-	    case 'update':
-	    	exec('cd screenshots && rm ' + filenames,
-	    		capture.bind(null, blocks));
-		    break;
-		case 'diff':
-			exec('cd failures && rm ' + filenames.replace('.png', '.fail.png'),
-				capture.bind(null, blocks.slice(), diff().bind(null, blocks)));
-			break;
-		default:
-			console.log('invalid argument');
+        case 'update':
+            exec('cd screenshots && rm ' + filenames,
+                capture.bind(null, blocks, done));
+            break;
+        case 'diff':
+            exec('cd failures && rm ' + filenames.replace('.png', '.fail.png'),
+                capture.bind(null, blocks.slice(), diff().bind(null, blocks)));
+            break;
+        default:
+            console.log('invalid argument');
     }
 }
