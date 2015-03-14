df84501811a5f4e9909e64525238dd072c161636
Katochimoto <bufpost@yandex.ru>
2015-03-14 15:04:05 +0300

    ошибка при выборе элемента саджеста

--- blocks/suggest/suggest.js
+++ blocks/suggest/suggest.js
@@ -231,7 +231,9 @@
 
             this.$jUI.on('suggestselect.nb-suggest', function(e, item) {
                 this.$selected = item.item;
-                this.input.setValue(item.item.value);
+                if (this.input) {
+                    this.input.setValue(item.item.value);
+                }
                 this.trigger('nb-select', this, item.item);
             }.bind(this));
 
